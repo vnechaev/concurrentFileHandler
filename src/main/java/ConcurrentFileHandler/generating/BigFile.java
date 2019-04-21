867d8a275e48ba425ee8fae1727fe45cc4611ce7
vsevolod <vsevolod.nechaev@phystech.edu>
2019-04-21 22:08:28 +0300

    refactor creating file

--- src/main/java/ConcurrentFileHandler/generating/BigFile.java
+++ src/main/java/ConcurrentFileHandler/generating/BigFile.java
@@ -3,20 +3,21 @@ package ConcurrentFileHandler.generating;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
-import org.apache.commons.io.FileUtils;
 
 public class BigFile {
 
     public static final int LINES_STEP_CHECKING_FILE_SIZE = 20000;
+    public static final int LINE_LENGTH = 100;
 
-    public static void createRandomAlphabetTextFile(String fileName) {
-        Random random = new Random();
-
+    public static String createRandomAlphabetTextFile(String fileName) {
         //Size in Mbs of file that will created
         double wantedSize = 11;
+        return createRandomAlphabetTextFile(fileName, wantedSize);
+    }
 
+    public static String createRandomAlphabetTextFile(String fileName, double wantedSize) {
+        Random random = new Random();
         File file = new File(fileName);
-        long start = System.currentTimeMillis();
         PrintWriter writer = null;
         try {
             writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8)), false);
@@ -25,13 +26,12 @@ public class BigFile {
         }
         int counter = 0;
         while (true) {
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < LINE_LENGTH; i++) {
                 writer.print((char) (random.nextInt(26) + 'a'));
             }
             writer.println();
             //Check if the current size is desirable
             if (++counter == LINES_STEP_CHECKING_FILE_SIZE) {
-                System.out.println(String.format("Size: %s", FileUtils.byteCountToDisplaySize(file.length())));
                 if (file.length() >= wantedSize * 1024 * 1024) {
                     writer.close();
                     break;
@@ -40,7 +40,6 @@ public class BigFile {
                 }
             }
         }
-        long time = System.currentTimeMillis() - start;
-        System.out.printf("Took %.1f seconds to createRandomAlphabetTextFile a file of %s", time / 1e3, FileUtils.byteCountToDisplaySize(file.length()));
+        return file.getPath();
     }
 }
